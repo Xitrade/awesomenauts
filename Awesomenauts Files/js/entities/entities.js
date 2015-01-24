99614ecbb46200a4332755fa0ab0e7e7325afc75
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-01-23 17:10:16 -0800

    added entity(player). getting error {Uncaught melonJS-1.1.0-min.js:10 TypeError: Cannot read property 'bind' of undefined}

--- Awesomenauts Files/js/entities/entities.js
+++ Awesomenauts Files/js/entities/entities.js
@@ -1,1 +1,18 @@
-// TODO
+game.playerEntity = me.Entity.extend({
+	init: function(x, y, settings){
+		this._super(me.Entity, 'init', [x, y, {
+			image: "player",
+			width: 64,
+			hieght: 64,
+			spritewidth: "64",
+			spriteheight: "64",
+			getShape: function(){
+				return(new me.Rect(0, 0, 64, 64)).toPolygon();
+			}
+		}]);
+	},
+
+	update: function (){
+
+	}
+});
--- Awesomenauts Files/js/resources.js
+++ Awesomenauts Files/js/resources.js
@@ -4,7 +4,9 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-{name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+	{name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+	{name: "player", type:"image", src: "data/img/skeleton.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
