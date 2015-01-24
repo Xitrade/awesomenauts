8d65afca4504eba4920b2b0750b1cf2b6b8f244e
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-01-23 17:50:50 -0800

    !error still present! updated entities so that player would be able to move

--- Awesomenauts Files/js/entities/entities.js
+++ Awesomenauts Files/js/entities/entities.js
@@ -3,16 +3,26 @@ game.playerEntity = me.Entity.extend({
 		this._super(me.Entity, 'init', [x, y, {
 			image: "player",
 			width: 64,
-			hieght: 64,
+			height: 64,
 			spritewidth: "64",
 			spriteheight: "64",
 			getShape: function(){
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
+
+		this.body.setVelocity(5, 0);
+
 	},
 
 	update: function (){
-
+		if(me.input.isKeyPressed("right")){
+			//sets position of x by adding the velocity difined above in "setVelocity"
+			this.body.vel.x += this.body.accel.x = me.timer.tick;
+		}else{
+			this.body.vel.x = 0;
+		}
+		this.body.update(delta);
+		return true;
 	}
 });
