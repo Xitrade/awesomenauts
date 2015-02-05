f1293fba87480568c051b7aede634c6bdcf2c4d6
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-02-05 11:40:58 -0800

    added 2 new characters

--- Awesomenauts Files/js/entities/entities.js
+++ Awesomenauts Files/js/entities/entities.js
@@ -12,9 +12,11 @@ game.PlayerEntity = me.Entity.extend({
 		}]);
 
 		this.body.setVelocity(5, 20);
+		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
 		this.renderable.addAnimation("idle", [78]);
 		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+		this.renderable.addAnimation("attack", [], 80);
 
 		this.renderable.setCurrentAnimation("idle");
 
@@ -29,7 +31,9 @@ game.PlayerEntity = me.Entity.extend({
 			this.body.vel.x = 0;
 		}
 
-		if(!this.body.vel.x !== 0){
+		if(me.input.isKeyPressed("attack"))
+
+		if(this.body.vel.x !== 0){
 			if(!this.renderable.isCurrentAnimation("walk")){
 				this.renderable.setCurrentAnimation("walk");
 			}
@@ -42,4 +46,88 @@ game.PlayerEntity = me.Entity.extend({
 		this._super(me.Entity, "update", [delta]);
 		return true;
 	}
+});
+
+game.PlayerBaseEntity = me.Entity.extend({
+	init : function(x, y, settings){
+		this._super(me.Entity, 'init', [x, y, {
+			image: "tower",
+			width: 100,
+			height: 100,
+			spritewidth: "100",
+			spriteheight: "100",
+			getShape: function(){
+				return (new me.Rect(0, 0, 100, 100)).toPolygon();
+			}
+
+		}]);
+		this.broken = false;
+		this.health = 10;
+		this.alwaysUpdate = true;
+		this.body.onCollision = this.onCollision.bind(this);
+	
+		this.type = "PlayerBaseEntity";
+
+		this.renderable.addAnimation("idle", [0]);
+		this.renderable.addAnimation("broken", [1]);
+		this.renderable.setCurrentAnimation("idle");
+	},
+
+	update:function(delta){
+		if(this.health<=0){
+			this.broken = true;
+			this.renderable.setCurrentAnimation("broken");
+		}
+		this.body.update(delta);
+
+		this._super(me.Entity, "update", [delta]);
+		return true;
+	},
+
+	onCollision: function(){
+
+	}
+
+});
+
+game.EnemyBaseEntity = me.Entity.extend({
+	init : function(x, y, settings){
+		this._super(me.Entity, 'init', [x, y, {
+			image: "tower",
+			width: 100,
+			height: 100,
+			spritewidth: "100",
+			spriteheight: "100",
+			getShape: function(){
+				return (new me.Rect(0, 0, 100, 100)).toPolygon();
+			}
+
+		}]);
+		this.broken = false;
+		this.health = 10;
+		this.alwaysUpdate = true;
+		this.body.onCollision = this.onCollision.bind(this);
+	
+		this.type = "EnemyBaseEntity";
+
+		this.renderable.addAnimation("idle", [0]);
+		this.renderable.addAnimation("broken", [1]);
+		this.renderable.setCurrentAnimation("idle");
+	},
+
+	update:function(delta){
+		if(this.health<=0){
+			this.broken = true;
+			this.renderable.setCurrentAnimation("broken");
+		}
+		this.body.update(delta);
+
+		this._super(me.Entity, "update", [delta]);
+		return true;
+	},
+
+	onCollision: function(){
+		
+	}
+
 });
--- Awesomenauts Files/js/resources.js
+++ Awesomenauts Files/js/resources.js
@@ -6,7 +6,8 @@ game.resources = [
 	 */
 	{name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
 	{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
-	{name: "player", type:"image", src: "data/img/goldKnight.png"},
+	{name: "player", type:"image", src: "data/img/warTornArcher.png"},
+	{name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
