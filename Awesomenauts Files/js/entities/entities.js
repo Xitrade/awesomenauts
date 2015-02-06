79c8769da0b6c2fc71e205d5763c176e4957a7db
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-02-05 19:34:48 -0800

    did video 20. nothing seems to be updating... same problems from before

--- Awesomenauts Files/js/entities/entities.js
+++ Awesomenauts Files/js/entities/entities.js
@@ -14,6 +14,9 @@ game.PlayerEntity = me.Entity.extend({
 		this.body.setVelocity(5, 20);
 		//keeps track of which direction your character is going
 		this.facing = "right";
+		this.now = new Date().getTime();
+		this.lastHit = this.now;
+		this.lastAttack = new Date().getTime();
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
 		this.renderable.addAnimation("idle", [143]);
@@ -25,6 +28,7 @@ game.PlayerEntity = me.Entity.extend({
 	},
 
 	update: function(delta){
+		this.now = new Date().getTime();
 		if(me.input.isKeyPressed("right")){
 			//sets position of x by adding the velocity difined above in "setVelocity"
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
@@ -74,7 +78,7 @@ game.PlayerEntity = me.Entity.extend({
 	},
 
 	collideHandler: function(response){
-		if (response.b.type=== 'EnemyBaseEntity') {
+		if(response.b.type=== 'EnemyBaseEntity') {
 			var ydif = this.pos.y - response.b.pos.y;
 			var xdif = this.pos.x - response.b.pos.x;
 		
@@ -89,6 +93,12 @@ game.PlayerEntity = me.Entity.extend({
 				this.body.vel.x = 0;
 				this.pos.x = this.pos.x +1;
 
+			}
+
+			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 600){
+				
+				this.lastHit = this.now;
+				response.b.loseHealth();
 			}
 		}
 	}
@@ -175,6 +185,68 @@ game.EnemyBaseEntity = me.Entity.extend({
 
 	onCollision: function(){
 		
+	},
+
+	loseHealth: function() {
+		this.health--;
 	}
 
+});
+
+game.EnemyCreep = me.Entity.extend({
+		init: function(x, y, settings) {
+		this._super(me.Entity, 'init', [x, y, {
+			image: "creep1",
+			width: 32,
+			height: 64,
+			spritewidth: "32",
+			spriteheight: "64",
+			getShape: function() {
+				return (new me.Rect(0, 0, 32, 64)).toPolygon();
+			}
+		}]);
+		//gives health
+		this.health = 10;
+		this.alwaysUpdate = true;
+
+		this.body.setVelocity(3, 20);
+
+		this.type = "EnemyCreep";
+
+		this.renderable.addAnimation("walk", [3, 4, 5], 80);
+		this.renderable.setCurrentAnimation("walk");
+
+	},	
+
+	update: function(delta) {
+		this.body.vel.x -= this.body.accel.x * me.timer.tick;
+
+		this.body.update(delta);
+	
+
+		this._super(me.Entity, "update", [delta]);
+
+		return true;
+	}
+});
+
+game.GameManager = Object.extend({
+	init: function(x, y, settings) {
+		this.now = new Date().getTime();
+		this.lastCreep = new Date().getTime();
+
+		this.alwaysUpdate = true;
+	},
+
+	update: function() {
+		this.now = new Date().getTime();
+
+		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)) {
+			this.lastCreep = this.now;
+			var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+			me.game.world.addChild(creepe, 5);
+		}
+
+		return true;
+	}
 });
