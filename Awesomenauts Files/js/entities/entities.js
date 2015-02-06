db45ada335f2f533f76dfc346c633e0af38d9602
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-02-05 19:08:36 -0800

    added another character. jump not working properly. attack does not work, only works when character is moving but doesnt play animation, just twitches

--- Awesomenauts Files/js/entities/entities.js
+++ Awesomenauts Files/js/entities/entities.js
@@ -12,11 +12,13 @@ game.PlayerEntity = me.Entity.extend({
 		}]);
 
 		this.body.setVelocity(5, 20);
+		//keeps track of which direction your character is going
+		this.facing = "right";
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
 
-		this.renderable.addAnimation("idle", [78]);
-		this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
-		this.renderable.addAnimation("attack", [], 80);
+		this.renderable.addAnimation("idle", [143]);
+		this.renderable.addAnimation("walk", [143, 144, 145, 146, 147, 148, 149, 150, 151], 80);
+		this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
 
 		this.renderable.setCurrentAnimation("idle");
 
@@ -26,26 +28,71 @@ game.PlayerEntity = me.Entity.extend({
 		if(me.input.isKeyPressed("right")){
 			//sets position of x by adding the velocity difined above in "setVelocity"
 			this.body.vel.x += this.body.accel.x * me.timer.tick;
+			this.facing = "right";
+			this.flipX(false);
+		}else if(me.input.isKeyPressed("left")){
+			this.facing = "left";
+			this.body.vel.x -= this.body.accel.x * me.timer.tick;
 			this.flipX(true);
 		}else{
 			this.body.vel.x = 0;
 		}
 
-		if(me.input.isKeyPressed("attack"))
 
-		if(this.body.vel.x !== 0){
+		if(me.input.isKeyPressed("jump") && !this.jumping && !this.body.falling){
+			this.jumping = true;
+			this.body.vel.y -= this.body.accel.y * me.timer.tick;
+		}
+
+
+		if(me.input.isKeyPressed("attack")){
+			if(!this.renderable.isCurrentAnimation("attack")){
+				//sets current animation to attack
+				//then returns to idle
+				this.renderable.isCurrentAnimation("attack", "idle");
+				//the next time we start this sequence
+				//we begin from the first animation, not where we left off
+				//when we switched to another animation
+				this.renderable.setAnimationFrame();
+			}
+		}
+
+
+		else if(this.body.vel.x !== 0 && !this.renderable.isCurrentAnimation("attack")){
 			if(!this.renderable.isCurrentAnimation("walk")){
 				this.renderable.setCurrentAnimation("walk");
 			}
-		}else{
+		}else if(!this.renderable.isCurrentAnimation("attack")){
 			this.renderable.setCurrentAnimation("idle");
 		}
 
+		me.collision.check(this, true, this.collideHandler.bind(this), true);
 		this.body.update(delta);
 		
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+	collideHandler: function(response){
+		if (response.b.type=== 'EnemyBaseEntity') {
+			var ydif = this.pos.y - response.b.pos.y;
+			var xdif = this.pos.x - response.b.pos.x;
+		
+			if(ydif<-40 && xdif< 70 && xdif>-35){
+				this.body.falling = false;
+				this.body.vel.y = -1;
+			}
+			else if (xdif>-35 && this.facing==='right' && (xdif<0) && ydif>-50){
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x -1;
+			}else if(xdif<70 && this.facing==='left' && (xdif>0)){
+				this.body.vel.x = 0;
+				this.pos.x = this.pos.x +1;
+
+			}
+		}
 	}
+
 });
 
 game.PlayerBaseEntity = me.Entity.extend({
@@ -57,7 +104,7 @@ game.PlayerBaseEntity = me.Entity.extend({
 			spritewidth: "100",
 			spriteheight: "100",
 			getShape: function(){
-				return (new me.Rect(0, 0, 100, 100)).toPolygon();
+				return (new me.Rect(0, 0, 100, 80)).toPolygon();
 			}
 
 		}]);
@@ -99,7 +146,7 @@ game.EnemyBaseEntity = me.Entity.extend({
 			spritewidth: "100",
 			spriteheight: "100",
 			getShape: function(){
-				return (new me.Rect(0, 0, 100, 100)).toPolygon();
+				return (new me.Rect(0, 0, 100, 80)).toPolygon();
 			}
 
 		}]);
