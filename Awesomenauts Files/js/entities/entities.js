755361807b0296eceb757f48b129b883ad562b5d
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-02-27 19:25:46 -0800

    finished video 33

--- Awesomenauts Files/js/entities/entities.js
+++ Awesomenauts Files/js/entities/entities.js
@@ -43,6 +43,7 @@ game.PlayerEntity = me.Entity.extend({
 		//keeps track of which direction your character is going
 		this.facing = "right";
 		this.dead = false;
+		this.attacking = false;
 	},
 
 	addAnimation: function(){
@@ -58,26 +59,7 @@ game.PlayerEntity = me.Entity.extend({
 
 		this.checkKeyPressesAndMove();
 
-		if(me.input.isKeyPressed("attack")){
-			if(!this.renderable.isCurrentAnimation("attack")){
-				//sets current animation to attack
-				//then returns to idle
-				this.renderable.setCurrentAnimation("attack", "idle");
-				//the next time we start this sequence
-				//we begin from the first animation, not where we left off
-				//when we switched to another animation
-				this.renderable.setAnimationFrame();
-			}
-		}
-
-
-		else if(this.body.vel.x !== 0 && !this.renderable.isCurrentAnimation("attack")){
-			if(!this.renderable.isCurrentAnimation("walk")){
-				this.renderable.setCurrentAnimation("walk");
-			}
-		}else if(!this.renderable.isCurrentAnimation("attack")){
-			this.renderable.setCurrentAnimation("idle");
-		}
+		this.setAnimation();
 
 		me.collision.check(this, true, this.collideHandler.bind(this), true);
 		this.body.update(delta);
@@ -105,6 +87,8 @@ game.PlayerEntity = me.Entity.extend({
 		if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling){
 			this.jump();
 		}
+
+		this.attacking = me.input.isKeyPressed("attack");
 	},
 
 	moveRight: function(){
@@ -125,62 +109,106 @@ game.PlayerEntity = me.Entity.extend({
 		this.body.vel.y -= this.body.accel.y * me.timer.tick;
 	},
 
+	setAnimation: function(){
+		if(this.attacking){
+			(!this.renderable.isCurrentAnimation("attack")){
+				//sets current animation to attack
+				//then returns to idle
+				this.renderable.setCurrentAnimation("attack", "idle");
+				//the next time we start this sequence
+				//we begin from the first animation, not where we left off
+				//when we switched to another animation
+				this.renderable.setAnimationFrame();
+			}
+		}
+
+		else if(this.body.vel.x !== 0 && !this.renderable.isCurrentAnimation("attack")){
+			if(!this.renderable.isCurrentAnimation("walk")){
+				this.renderable.setCurrentAnimation("walk");
+			}
+		}else if(!this.renderable.isCurrentAnimation("attack")){
+			this.renderable.setCurrentAnimation("idle");
+		}		
+	},
+
 	loseHealth: function(damage){
 		this.health = this.health - damage;
 	},
 
 	collideHandler: function(response){
 		if(response.b.type=== 'EnemyBaseEntity') {
-			var ydif = this.pos.y - response.b.pos.y;
-			var xdif = this.pos.x - response.b.pos.x;
-		
-			if(ydif<-40 && xdif< 70 && xdif>-35){
-				this.body.falling = false;
-				this.body.vel.y = -1;
-			}
-			else if (xdif>-35 && this.facing==='right' && (xdif<0) && ydif>-50){
-				this.body.vel.x = 0;
-				this.pos.x = this.pos.x -1;
-			}else if(xdif<70 && this.facing==='left' && (xdif>0)){
-				this.body.vel.x = 0;
-				this.pos.x = this.pos.x +1;
-
-			}
-
-			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer){
-				
-				this.lastHit = this.now;
-				response.b.loseHealth(game.data.playerAttack);
-			}
+			this.collideWithEnemyBase(response);
 		}else if(response.b.type=== 'EnemyCreep'){
-			var xdif = this.pos.x - response.b.pos.x;
-			var ydif = this.pos.y - response.b.pos.x;
+			this.collideWithEnemyCreep(response);
+		}
+	},
 
-			if (xdif>0) {
-				this.pos.x = this.pos.x + 1;
-				if(this.facing==="left"){
-					this.body.vel.x = 0;
-				}
-			}else{
-				this.pos.x = this.pos.x - 1;
-				if(this.facing==="right"){
-					this.body.vel.x = 0;
-				}
-			}
+	collideWithEnemyBase: function(response){
+		var ydif = this.pos.y - response.b.pos.y;
+		var xdif = this.pos.x - response.b.pos.x;
+	
+		if(ydif<-40 && xdif< 70 && xdif>-35){
+			this.body.falling = false;
+			this.body.vel.y = -1;
+		}
+		else if (xdif>-35 && this.facing==='right' && (xdif<0) && ydif>-50){
+			this.body.vel.x = 0;
+			this.pos.x = this.pos.x -1;
+		}else if(xdif<70 && this.facing==='left' && (xdif>0)){
+			this.body.vel.x = 0;
+			this.pos.x = this.pos.x +1;
 
-			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer
-				&& (Math.abs(ydif) <=40) && 
-				(((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right"))
-				){
-				this.lastHit = this.now;
-				if (response.b.health <= game.data.playerAttack) {
-					game.data.gold += 1;
-					console.log("Current Gold: " + game.data.gold);
-				}
-				response.b.loseHealth(game.data.playerAttack);
-			}
+		}
+
+		if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer){
+			this.lastHit = this.now;
+			response.b.loseHealth(game.data.playerAttack);
 		}
 	}
 
-});
+	collideWithEnemyCreep: function(response){
+		var xdif = this.pos.x - response.b.pos.x;
+		var ydif = this.pos.y - response.b.pos.x;
 
+		this.stopMovement(xdif);
+			
+			if (this.checkAttack(xdif, ydif))
+				this.hitCreep(response);	
+		};
+	},
+
+	stopMovement: function(xdif){
+		if (xdif>0) {
+			this.pos.x = this.pos.x + 1;
+				if(this.facing==="left"){
+					this.body.vel.x = 0;
+			}
+		}else{
+			this.pos.x = this.pos.x - 1;
+				if(this.facing==="right"){
+					this.body.vel.x = 0;
+			}
+		}
+	},
+
+	checkAttack: function(xdif, ydif){
+		if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer
+			&& (Math.abs(ydif) <=40) && 
+			(((xdif>0) && this.facing==="left") || ((xdif<0) && this.facing==="right"))
+			){
+			this.lastHit = this.now;
+			return true;
+		}	
+		return false;	
+	},
+
+	hitCreep: function(response){
+		if(this.checkAttack(xdif, ydif)){
+		if (response.b.health <= game.data.playerAttack) {
+			game.data.gold += 1;
+			console.log("Current Gold: " + game.data.gold);
+		}
+		response.b.loseHealth(game.data.playerAttack);	
+	}
+
+});
