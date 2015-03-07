fc79369f6f4e9b376e129435fb13bc3a706fba57
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-03-06 18:50:03 -0800

    updated music. finished video 41

--- Awesomenauts Files/js/entities/GameManager.js
+++ Awesomenauts Files/js/entities/GameManager.js
@@ -47,18 +47,32 @@ game.HeroDeathManager = Object.extend({
 game.ExperienceManager = Object.extend({
 	init: function(x, y, settings){
 		this.alwaysUpdate = true;
+		this.gameOver = false;
 	},
 
 	update: function(){
 		//exp for winning
-		if(game.data.win === true){
-			game.data.exp += 10;
+		if(game.data.win === true && !this.gameover){
+			this.gameOver(true);
 		//exp for losing
-		}else if(game.data.win === false){
-			game.data.exp += 5;
+		}else if(game.data.win === false && !this.gameover){
+			this.gameOver(false);
 		}
+		console.log(game.data.exp);
 
 		return true;
+	},
+
+	gameOver: function(win){
+		if (win) {
+			game.data.exp += 10;		
+		}else{
+			game.data.exp += 5;			
+		}
+
+		this.gameover = true;
+		me.save.exp = game.data.exp;	
+		me.save.exp2 = 4;
 	}
 
 });
